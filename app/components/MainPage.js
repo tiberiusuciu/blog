4042b0048228a8685a8395e5b991642d926ff3f2
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-06 21:52:55 -0500

    Added navbar styling and a first look at it's appearance

--- app/components/MainPage.js
+++ app/components/MainPage.js
@@ -9,10 +9,12 @@ class MainPage extends Component {
 	render() {
 		return (
 			<div className={"row " + styles.mainRowStyling}>
+				<NavigatorBar />
 				<div className="col" ></div>
 				<div className={"col-8 " + styles.noPadding}>
-					<NavigatorBar />
-					<PostList />
+					<div className={"row " + styles.mainRowStyling}>
+						<PostList />
+					</div>
 				</div>
 				<div className="col"></div>
 			</div>
--- app/components/NavigatorBar.js
+++ app/components/NavigatorBar.js
@@ -6,7 +6,21 @@ class NavigatorBar extends Component {
 	render() {
 		return (
 			<div className={"row " + styles.navigatorBar + " " + styles.mainRowStyling}>
-				<h1>Hello!!!</h1>
+        <div className={"col " + styles.noPadding}></div>
+				<div className={"col-8 align-self-center " + styles.noPadding}>
+          <div className={"row " + styles.mainRowStyling}>
+            <div className={"col " + styles.navButton}>ABOUT</div>
+            <div className={"col " + styles.navButton}>IMAGES</div>
+            <div className={"col " + styles.navButton}>EVENT</div>
+            <div className={"col-4"}>
+              <h1>TIBERIUS</h1>
+            </div>
+            <div className={"col " + styles.navButton}>GAMES</div>
+            <div className={"col " + styles.navButton}>DEV</div>
+            <div className={"col " + styles.navButton}>MISC</div>
+          </div>
+        </div>
+        <div className={"col " + styles.noPadding}></div>
 			</div>
 		)
 	}
