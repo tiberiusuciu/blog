4ec9a733524c63f5ccdc220c296380346154e745
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-08 00:05:34 -0500

    Added an example on how rating would appear, WIP

--- app/components/SinglePost.js
+++ app/components/SinglePost.js
@@ -14,7 +14,9 @@ class SinglePost extends Component {
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus sapien, convallis lobortis tristique fringilla, tristique id velit. Nulla eros tellus, gravida ac consectetur id, commodo ut quam. Ut mollis orci id libero aliquet, in blandit orci mollis. Praesent non volutpat leo, id rutrum ipsum. Aenean dictum mauris ac risus interdum, ut porttitor sapien vestibulum. Integer dictum tellus at posuere gravida. Suspendisse suscipit augue eu tellus suscipit, vel interdum dolor interdum. Nunc vitae viverra mi. Sed lorem ligula, faucibus sed dapibus sed, posuere nec l ectus.</p>
 					</div>
 					<div className={styles.singlePostRatingsStyling}>
-						+
+						<div className={styles.ratingButtons + " " + styles.ratingAdd}>+</div>
+						<div className={styles.ratingButtons + " " + styles.ratingComment}>177</div>
+						<div className={styles.ratingButtons + " " + styles.ratingRemove}>-</div>
 					</div>
 				</div>
 			</div>
