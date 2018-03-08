54de24b38f6948e91d32df9bff1a4d758a762433
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-07 23:35:57 -0500

    First attempt at adding rating bar

--- app/components/SinglePost.js
+++ app/components/SinglePost.js
@@ -9,8 +9,13 @@ class SinglePost extends Component {
 		return (
 			<div className={"row " + styles.singlePostStyling + " " + styles.mainRowStyling}>
 				<div className={"col-12 " + styles.noPadding}>
-					<h2>This is a post!</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus sapien, convallis lobortis tristique fringilla, tristique id velit. Nulla eros tellus, gravida ac consectetur id, commodo ut quam. Ut mollis orci id libero aliquet, in blandit orci mollis. Praesent non volutpat leo, id rutrum ipsum. Aenean dictum mauris ac risus interdum, ut porttitor sapien vestibulum. Integer dictum tellus at posuere gravida. Suspendisse suscipit augue eu tellus suscipit, vel interdum dolor interdum. Nunc vitae viverra mi. Sed lorem ligula, faucibus sed dapibus sed, posuere nec lectus.</p>
+					<div className={styles.singlePostTextStyling}>
+						<h2>This is a post!</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin risus sapien, convallis lobortis tristique fringilla, tristique id velit. Nulla eros tellus, gravida ac consectetur id, commodo ut quam. Ut mollis orci id libero aliquet, in blandit orci mollis. Praesent non volutpat leo, id rutrum ipsum. Aenean dictum mauris ac risus interdum, ut porttitor sapien vestibulum. Integer dictum tellus at posuere gravida. Suspendisse suscipit augue eu tellus suscipit, vel interdum dolor interdum. Nunc vitae viverra mi. Sed lorem ligula, faucibus sed dapibus sed, posuere nec l ectus.</p>
+					</div>
+					<div className={styles.singlePostRatingsStyling}>
+						+
+					</div>
 				</div>
 			</div>
 		)
