0261494e69899177d6251d31fd385cf5dc8e830c
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-07 21:24:00 -0500

    Started adding dashboard styling, which are essentially important news or some sort

--- app/components/MainPage.js
+++ app/components/MainPage.js
@@ -10,13 +10,26 @@ class MainPage extends Component {
 		return (
 			<div className={"row " + styles.mainRowStyling}>
 				<NavigatorBar />
-				<div className="col" ></div>
-				<div className={"col-8 " + styles.noPadding}>
-					<div className={"row " + styles.mainRowStyling}>
-						<PostList />
+				<div className={"col-12 " + styles.noPadding}>
+				<div className={"row " + styles.mainRowStyling + " " + styles.dashboardBlock}>
+					<div className={"col-12 " + styles.noPadding}>
+						<div className={"row " + styles.mainRowStyling}>
+							<div>This is a test</div>
+						</div>
 					</div>
 				</div>
-				<div className="col"></div>
+				</div>
+				<div className={"col-12 " + styles.noPadding}>
+					<div className={"row " + styles.mainRowStyling}>
+						<div className="col" ></div>
+						<div className={"col-8 " + styles.noPadding}>
+							<div className={"row " + styles.mainRowStyling}>
+								<PostList />
+							</div>
+						</div>
+						<div className="col"></div>
+					</div>
+				</div>
 			</div>
 		)
 	}
